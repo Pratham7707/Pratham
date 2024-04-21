e3b55c92105236dba7e148aae2454333c1c186da
Pratham7707 <130427895+Pratham7707@users.noreply.github.com>
2024-04-21 18:17:21 +0530

    Add files via upload

---
+++ instagram.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3307
+-- Generation Time: Apr 21, 2024 at 02:45 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `instagram`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instagram`
+--
+
+CREATE TABLE `instagram` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `instagram`
+--
+
+INSERT INTO `instagram` (`username`, `password`) VALUES
+('', ''),
+('', ''),
+('', ''),
+('', ''),
+('', ''),
+('', ''),
+('', ''),
+('dggfgdf', 'sdffgdgdfg'),
+('dggfgdf', 'sdffgdgdfg'),
+('dggfgdf', 'dffsf'),
+('dggfgdf', 'dffsf'),
+('dggfgdf', 'dvdfgf'),
+('giorjgjjoi', 'dnfoioijio'),
+('pratham', 'Pratham133');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
